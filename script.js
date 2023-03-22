061a2f9e012f593da63fbfd79aa5f681e6e38020
Inzamamul Hasan <107973468+InzamamulHasan@users.noreply.github.com>
2023-03-22 12:14:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+	let i=0;
+	let j=str.length-1;
+	while(i<j){
+		if(str.chatAt(i)!=str.chatAt(j)){
+			return false;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
